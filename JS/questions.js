41df7959fc23d1741c6feabc6ade268025adcd1d
PedroPiovani017 <pedro.franca@sptech.school>
2024-05-29 00:31:25 -0300

    Começando quiz

---
+++ JS/questions.js
@@ -0,0 +1,92 @@
+export default [
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a afinação padrão de uma guitarra de 6 cordas?",
+        answers: [
+            {option: "E A D G B E", correct: true},
+            {option: "D G C F A D", correct: false},
+            {option: "C G C F A D", correct: false},
+            {option: "E A D G B D", correct: false},
+        ],
+    },
+]
